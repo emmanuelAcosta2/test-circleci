a91be9b9b2f22c5da2f947d308c7ace8efe6c65f
EmmanuelAcosta <emmanuel.acosta@iugo.com.uy>
2022-08-10 17:23:18 -0300

    primer test

---
+++ script.js
@@ -0,0 +1,34 @@
+const googleDatabase = [
+    'dog.com',
+    'cat.com',
+    'mouse.com',
+    'rabbit.com',
+    'hamster.com',
+    'gerbil.com',
+    'turtle.com',
+    'chinchilla.com',
+    'skunk.com',
+    'parrot.com',
+    'dogs.com',
+    'cats.com',
+    'mice.com',
+    'doggos.com',
+    'dogy.com',
+    'doggo.com',
+    'doggoes.com',
+    'doggoooooo.com',
+    'doggoooooooo.com',
+    'doggooooooooo.com',
+    'doggooooooooooo.com',
+    'doggoooooooooooo.com',
+];
+
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+    
+}
+
+console.log(googleSearch('dog', googleDatabase));

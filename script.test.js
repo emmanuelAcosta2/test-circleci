d8d1c5473ef5a28c980b52707e7cee3dfaa4c907
EmmanuelAcosta <emmanuel.acosta@iugo.com.uy>
2022-08-16 13:58:42 -0300

    otro fallido

--- script.test.js
+++ script.test.js
@@ -23,6 +23,7 @@ dbMock = [
 describe('googleSearch', () => {
     it('is a silly test', () => {
         expect(true).toBe(false);
+        expect(true).toBe(false);
         //googleSearch('Testtest',dbMock);
     })
     
